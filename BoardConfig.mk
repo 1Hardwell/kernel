fb3b0a2172b99937c1c829436a1c4bff41d411db
Ordenkrieger <OrdenKriegerEDR@googlemail.com>
2016-07-20 16:16:07 +0200

    ham: Include stock cyanogen firmware
     * Makes life easier for evryone
    
    Signed-off-by: OrdenKrieger <OrdenKriegerEDR@googlemail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -177,6 +177,9 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_ham
 TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 
+# Releasetools firmware
+ADD_RADIO_FILES := true
+
 # Use HW crypto for ODE
 TARGET_HW_DISK_ENCRYPTION := true
 
