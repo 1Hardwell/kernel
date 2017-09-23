1efa4adf450db400306095a13f8581bd65a20ae9
Luca Stefani <luca020400@lineageos.org>
2017-09-24 00:01:46 +0200

    Add HIDL manifest

--- ham.mk
+++ ham.mk
@@ -128,6 +128,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
 #    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
 
+# HIDL manifest
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
+
 # Lights
 PRODUCT_PACKAGES += \
     lights.msm8974
