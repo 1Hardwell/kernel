cf94fc7e8b8b40efef9553dc1db54bd0c84e29ed
ShubhamB6596 <shubhamprince111@gmail.com>
2018-01-19 17:27:21 +0000

    Revert "ham: Update for early-mounted system partition"
    This reverts commit 2f388d3ec95bf594ce15e3d87cebe978f458a894.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -194,7 +194,7 @@ TARGET_USES_INTERACTION_BOOST 	:= true
 TARGET_RIL_VARIANT := caf
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
